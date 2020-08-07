1691a0fba47c1ca162dd529c4e24dcd93264b324
rulflyfly <rulflyfly@gmail.com>
2020-08-07 16:35:21 +0300

    Initial Commit

---
+++ Bull&Cows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bull&Cows
+//
+//  Created by Anastasia on 07.08.2020.
+//  Copyright © 2020 Anastasia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
